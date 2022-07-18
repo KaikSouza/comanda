b920a9bbb4adcdb6501271a6961391c86da9f561
KaikSouza <kaiksouza2020@gmail.com>
2022-07-18 12:14:11 -0300

    alteração

---
+++ src/empresa/model/delete.php
@@ -0,0 +1,23 @@
+<?php
+
+    include('../../conexao/conexao.php');
+
+    $ID = $_REQUEST['ID'];
+
+    $sql = "DELETE FROM EMPRESA WHERE ID = $ID";
+
+    $resultado = $pdo->query($sql);
+
+    if($resultado){
+        $dados = array(
+            'tipo' => 'success',
+            'mensagem' => 'Empresa excluída com sucesso!'
+        );
+    }else{
+        $dados = array(
+            'tipo' => 'success',
+            'mensagem' => 'Não foi possível excluir a empresa!'
+        );
+    }
+
+    echo json_encode($dados);
